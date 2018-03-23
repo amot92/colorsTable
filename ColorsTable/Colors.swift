f6f5105a61c7f75b591a98a956667da48c535a46
Adam Oakes <amot92@mail.missouri.edu>
2018-03-22 19:28:33 -0500

    Added colors struct

---
+++ ColorsTable/Colors.swift
@@ -0,0 +1,14 @@
+//
+//  Colors.swift
+//  ColorsTable
+//
+//  Created by Adam Oakes on 3/22/18.
+//  Copyright © 2018 Adam Oakes. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    let name:String
+    let uiColor: UIColor
+}
